52d92bdc483ccc231ed84eae3aaa7f7c3307e911
Jacob Dyke <jacobdyke@test-161101.dhcp.missouri.edu>
2023-11-05 00:06:36 -0500

    Wifi start

--- JoyStickBluetooth/JoyStickBluetooth.ino
+++ JoyStickBluetooth/JoyStickBluetooth.ino
@@ -1,39 +1,44 @@
-#include <BluetoothSerial.h>
+#include <WiFi.h>
 
 int JoyStick_X = 4; //x
 int JoyStick_Y = 2; //y 4095
-bool check = false;
 
-BluetoothSerial SerialBT;
-void connectToSlave() {
-  // This should be the name or address of the Slave ESP32
-  SerialBT.connect("Boat");
-}
+const char *ssid = "Master"; // SSID of the access point to connect to
+const char *password = "LigmaLigma"; 
+
+WiFiServer server(80);
+
 void setup() {
 
   Serial.begin(115200);
-  Serial.print("Starting Bluetooth For Controller... \n");
 
-  SerialBT.begin("Controller");
-  while (!SerialBT.connect("Boat")) {
-    Serial.println("Attempting to connect to Boat...");
-    delay(2000); // Wait for 2 seconds before retrying
-  }
-  Serial.println("Connected to Boat!");
-  delay(5000);
-
+  WiFi.softAP(ssid, password);
+  server.begin();
+  
+  Serial.println("Access Point Started");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.softAPIP());
 }
 
 void loop() {
   int up, heading;
   up=analogRead(JoyStick_Y);
   heading=analogRead(JoyStick_X);
-  Serial.print(up);
-  Serial.print(", ");
-  Serial.println(heading);
+  WiFiClient client = server.available();
   
+  //Serial.print(up);
+  //Serial.print(", ");
+  //Serial.println(heading);
 
-  String joystickData = String(heading) + "," + String(up) + "\n";
-  SerialBT.print(joystickData);
+  if (client) { // If a new client connects,
+    Serial.println("New Client."); // print a message out in the serial port
+    while (client.connected()) { // loop while the client's connected
+      // Wait for the client to send data
+      if (client.available()) {
+
+      }
+    }
+  }
+
   delay(5000);
 }
--- MotorControlBoat/MotorControlBoat.ino
+++ MotorControlBoat/MotorControlBoat.ino
@@ -1,48 +1,38 @@
 #include <ESP32Servo.h>
-#include <BluetoothSerial.h>
+#include <WiFi.h>
 
 Servo myServo;
-BluetoothSerial SerialBT;
 int JoyStick_X = 27; //x
 int JoyStick_Y = 26; //y 4095
 int motorPin = 32; //Motor
 int servoPin = 33; //Servo pin
+const char *ssid = "Master"; // SSID of the access point to connect to
+const char *password = "LigmaLigma"; 
 
   void setup() 
 {
   pinMode(motorPin, OUTPUT);
   myServo.attach(servoPin);
   Serial.begin(115200); // 115200 bps
-  Serial.print("Starting Bluetooth For Boat... \n");
-  SerialBT.begin("Boat");
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
   delay(1000);
 }
+
 void loop() 
 {
   int up, heading;
-  //up=analogRead(JoyStick_Y);
-  //heading=analogRead(JoyStick_X);
-  //Serial.print(up);
-  //Serial.print(", ");
-  //Serial.print(heading);
-  //Serial.print("\n");
-  if (SerialBT.available()) {
-    // Read the incoming string until newline character
-    String data = SerialBT.readStringUntil('\n');
-    Serial.print(data);
-    
-    // Split the data into two parts based on the comma
-    int commaIndex = data.indexOf(',');
-    if (commaIndex != -1) {
-      // Parse the up value from the part before the comma
-      up = data.substring(0, commaIndex).toInt();
-      
-      // Parse the heading value from the part after the comma
-      heading = data.substring(commaIndex + 1).toInt();
-      
-      // Now you can use `up` and `heading` as the joystick values
-    }
-  }
+  up =
+  heading = 
   if(up == 4095){
     digitalWrite(motorPin, HIGH);
   }
@@ -50,11 +40,9 @@ void loop()
     digitalWrite(motorPin, LOW);
   }
   if (heading == 4095){
-    //servoAngle = map(heading, 0, 4095, 0, 0);
     myServo.write(45);
   }
   else if (heading < 200){
-    //servoAngle = map(heading, 0, 4095, 0, 180);
     myServo.write(135);
   }
   else{
